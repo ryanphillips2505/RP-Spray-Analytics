c6608d1e75d14178d8bb86a7fece5371df11d1ea
ryanphillips2505 <ryanphillips2505@gmail.com>
2026-01-30 09:11:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1301,7 +1301,7 @@ st.markdown(
     """
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Jersey+10&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Inter:wght@600;700;800&display=swap" rel="stylesheet">
 """,
     unsafe_allow_html=True,
 )
